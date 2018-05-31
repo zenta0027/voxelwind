651be9eeb7c8b080eb75d6e7e2f83144621440fb
Zentroll <zenta0027@kaist.ac.kr>
2018-05-31 20:46:47 +0900

    Add a test case

--- server/src/test/java/com/voxelwind/server/game/item/VoxelwindItemStackTest.java
+++ server/src/test/java/com/voxelwind/server/game/item/VoxelwindItemStackTest.java
@@ -1,6 +1,8 @@
 package com.voxelwind.server.game.item;
 
 import com.voxelwind.api.game.level.block.BlockTypes;
+import com.voxelwind.api.game.item.ItemStack;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -22,5 +24,22 @@ public class VoxelwindItemStackTest {
 
         assertNotEquals("Items with differing amounts are equal", stack1, stack2);
         assertTrue("Items with differing amounts but same other data are not similar", stack1.isSimiliarTo(stack2));
+    }
+
+    @Test
+    public void isSimiliarToIsReallyEqual() throws Exception {
+        VoxelwindItemStack stack1 = new VoxelwindItemStack(BlockTypes.DIRT, 1, null);
+
+        assertEquals("Items with fuck", stack1, stack1);
+        assertTrue("Items with fuck fuck", stack1.isSimiliarTo(stack1));
+    }
+
+    @Test
+    public void isSimiliarNotSameBlockType() throws Exception {
+        VoxelwindItemStack stack1 = new VoxelwindItemStack(BlockTypes.DIRT, 1, null);
+        VoxelwindItemStack stack2 = new VoxelwindItemStack(BlockTypes.AIR, 1, null);
+
+        assertNotEquals("Items with fuck", stack1, stack2);
+        assertFalse("Items with fuck fuck", stack1.isSimiliarTo(stack2));
     }
 }
